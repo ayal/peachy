a44028738aef8f2cd617d75c253535914751c8b1
Ayal Gelles <ayalgelles@Ayals-MacBook-Air.local>
2015-12-06 14:53:01 +0200

    init

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-$.getJSON('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://pitchfork.com/rss/reviews/best/albums/?callback=?', function(x){
+$.getJSON('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http%3A%2F%2Fpitchfork.com%2Frss%2Freviews%2Fbest%2Falbums%2F&callback=?', function(x){
     console.log(x);
 })
