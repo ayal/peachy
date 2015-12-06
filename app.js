5cca8b98c2f79fa7f81b2774ede05a553c20793e
Ayal Gelles <ayalgelles@Ayals-MacBook-Air.local>
2015-12-06 14:51:34 +0200

    init

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-$.getJSON('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://pitchfork.com/rss/reviews/best/albums/', function(x){
+$.getJSON('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://pitchfork.com/rss/reviews/best/albums/?callback=?', function(x){
     console.log(x);
 })
